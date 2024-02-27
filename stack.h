83a851d66d7f1b1fd101e1ed3b2948e1ca6bbacc
Misha Faruki <mfaruki@usc.edu>
2024-02-26 16:39:27 -0800

    templates

--- stack.h
+++ stack.h
@@ -6,20 +6,42 @@
 
 // Use inheritance from std::vector (choose public/private) as appropriate
 template <typename T>
-class Stack 
+class Stack : public std::vector<T>  
 {
 public:
-    Stack();
+    Stack() {
+        std::vector<T> {}
+    }
     ~Stack();
-    bool empty() const;
-    size_t size() const;
-    void push(const T& item);
-    void pop();  // throws std::underflow_error if empty
-    const T& top() const; // throws std::underflow_error if empty
-    // Add other members only if necessary
+    bool empty() const {
+        std::vector<T>::empty(); 
+    } 
 
-    // I just added this line
+    size_t size() const {
+       std::vector<T>::size(); 
+    }
+    void push(const T& item) {
+       insert(0,item);
+    }
+    void pop() {
+        if(std::vector<T>::empty()) { 
+            throw std::underflow_error(“empty”); 
+            }
+
+	    std::vector<T>::erase(0); 
+
+    }
+    const T& top() const {
+        if(data.empty()) { 
+            throw std::underflow_error("empty"); 
+        }
+	    std::vector<T>::front(); 
+    }
+ 
+
+  
 };
+
 
 
 #endif
