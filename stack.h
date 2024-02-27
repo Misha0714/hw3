0e15406405877daeb96a8918b3b425c16cb2fd58
Misha Faruki <mfaruki@usc.edu>
2024-02-27 00:53:41 +0000

    stack

--- stack.h
+++ stack.h
@@ -8,34 +8,35 @@
 template <typename T>
 class Stack : public std::vector<T>  
 {
+private: 
+
 public:
     Stack() {
-        std::vector<T> {}
     }
-    ~Stack();
+    ~Stack() {}
+		
     bool empty() const {
-        std::vector<T>::empty(); 
+      	return std::vector<T>::empty(); 
     } 
 
     size_t size() const {
-       std::vector<T>::size(); 
+      	return std::vector<T>::size(); 
     }
     void push(const T& item) {
-       insert(0,item);
+       std::vector<T>::insert(std::vector<T>::begin(),item);
     }
     void pop() {
         if(std::vector<T>::empty()) { 
-            throw std::underflow_error(“empty”); 
-            }
+            throw std::underflow_error("empty"); 
+				}
 
-	    std::vector<T>::erase(0); 
-
+	    	std::vector<T>::erase(std::vector<T>::begin()); 
     }
     const T& top() const {
-        if(data.empty()) { 
-            throw std::underflow_error("empty"); 
-        }
-	    std::vector<T>::front(); 
+			if(std::vector<T>::empty()) { 
+					throw std::underflow_error("empty"); 
+			}
+	    return std::vector<T>::front(); 
     }
  
 
